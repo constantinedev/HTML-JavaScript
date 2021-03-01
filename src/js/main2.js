b39a2c0dbc2283f035aa944b3cdbf6c971db74fd
Constantine Dev <1324252+constantinedev@users.noreply.github.com>
2021-03-01 12:07:01 +0800

    Add files via upload

---
+++ src/js/main2.js
@@ -0,0 +1,208 @@
+//特殊功能
+function chgSTY(style) {
+    document.getElementById("result").value = style;
+};
+
+//圖片上傳 - [開始]
+function readURL(input) {
+    if (input.files && input.files[0]) {
+        var reader = new FileReader();
+        reader.onload = function(e) {
+            $("#blah").attr('src', e.target.result);
+            $("#blah").attr('width', '240px');
+            $('#imgupdate').on('click', function() {
+                alert("點擊圖片上傳成功");
+                $("#img1").attr('src', e.target.result);
+                $("#img1").attr('width', "240px");
+            });
+        };
+        reader.readAsDataURL(input.files[0]);
+    };
+};
+//圖片上傳 - [結束]
+
+jQuery(document).ready(function() {
+    //CSS設定
+    jQuery('.configure_editor').css({
+        'min-height': '600px',
+    });
+    jQuery('.contact').css({
+        'border-radius': '12px',
+        'margin-top': '150px',
+        'background-repeat': 'no-repeat',
+        'background-size': '100%',
+        'margin': '',
+    });
+
+    jQuery('[data-whatever="@styleSelect"]').click(function() {
+        console.log("OK button correct1");
+        //彈出視窗CSS調整
+        jQuery('.modal-title').html('<h4>樣式選擇</h4>');
+        jQuery('.mod_styleSelect').css({
+            'text-align': 'center',
+        });
+        jQuery('.mod_styleSelect').show();
+        jQuery('.mod_dynamicSelect').hide();
+        jQuery('.mod_FreeEditor').hide();
+        jQuery('.mod_Calendar').hide();
+        jQuery('.mod_self_carousel').hide();
+
+        //載入指定頁面- src/index/???.html
+        jQuery('.contact').html('<div class="cont"></div>');
+        jQuery('.cont').load('src/index/contact.html');
+        jQuery('.cont').css({
+            'margin': '10%',
+        });
+
+        //特定載入功能
+        jQuery('.mod_styleSelect').html('<input type="text" id="result" style="display: none;" />');
+        jQuery('.modal-footer').html('<button type="button" class="styupdate btn btn-primary float-right" id="styupdate">確定</button>');
+
+        jQuery('.styupdate').click(function() {
+            var style = document.getElementById('result').value;
+            jQuery('.contact').css({
+                'background-image': 'url(' + style + ')',
+            });
+        });
+
+        //樣式選擇API導入 - [開始]
+        var SvrURL = "https://testapi.ladraw.com:20102/Services/APIService.asmx/";
+        var API = "GetContactBackgroundTemplate";
+        jQuery.ajax({
+            url: SvrURL + API,
+            type: "POST",
+            dataType: "JSON",
+            ContentType: "application/x-www-form-urlencoded",
+            success: function(result, xhr, status, error) {
+                var len = result.Template.length;
+                for (let i = 0; i < len; i++) {
+                    var id = result.Template[i].TemplateID;
+                    var IMG = result.Template[i].Image;
+                    var sty = result.Template[i].Style;
+
+                    jQuery('.mod_styleSelect').append(
+                        '<label class="styobj" for="' + id + '">\
+                            <input type="image" class="btn-check" name="style" id="' + id + '" value="' + IMG + '" onclick="chgSTY(this.value)" src="' + IMG + '" style="width:200px;border-radius:12px;padding:0.5em;border-radius: 10px;">\
+                        </label>'
+                    );
+                };
+            },
+            error: function(xhr, status, error) {
+                console.log(error);
+            },
+        });
+        //樣式選擇API導入 - [結束]
+    });
+
+    //彈性編輯設定 - [開始]
+    jQuery('[data-whatever="@dynamicSelect"]').click(function() {
+
+        console.log("OK button correct2");
+        //彈出視窗CSS調整
+        jQuery('.modal-title').html('<h4>彈性編輯</h4>');
+        jQuery('.mod_styleSelect').hide();
+        jQuery('.mod_dynamicSelect').show();
+        jQuery('.mod_FreeEditor').hide();
+        jQuery('.mod_Calendar').hide();
+        jQuery('.mod_self_carousel').hide();
+
+        //特定載入功能
+        jQuery('.modal-footer').html('<button type="button" class="dynamicSelect btn btn-primary float-right" id="dynamicSelect">確定</button>');
+
+        //次選單
+        jQuery('.mod_dynamicSelect').html('<div class="menu1"></div><div class="sub_settings"></div>');
+        jQuery('.menu1').css('text-align', 'center');
+        jQuery('.modal-body .menu1').html(
+            '<div class="btn-group btn-group-toggle" data-toggle="buttons">\
+                <button class="btn btn-primary">顏色選擇</button>\
+                <button class="btn btn-primary">上傳背景圖片</button>\
+                <button class="btn btn-primary">上傳背景素材</button>\
+            </biv>');
+        jQuery('.sub_settings').html('Readt Test');
+
+    });
+    //彈性編輯設定 - [結束]
+
+    //自定編輯設定 - [開始]
+    jQuery('[data-whatever="@FreeEditor"]').click(function() {
+        console.log("OK button correct3");
+        //彈出視窗CSS調整
+        jQuery('.modal-title').html('<h4>自定編輯</h4>');
+        jQuery('.mod_styleSelect').hide();
+        jQuery('.mod_dynamicSelect').hide();
+        jQuery('.mod_FreeEditor').show();
+        jQuery('.mod_Calendar').hide();
+        jQuery('.mod_self_carousel').hide();
+
+        //特定載入功能
+        jQuery('.modal-footer').html('<button type="button" class="FreeEditor btn btn-primary float-right" id="FreeEditor">確定</button>');
+    });
+    //自定編輯設定 - [結束]
+
+    //行事曆設定 - [開始]
+    jQuery('[data-whatever="@Calendar"]').click(function() {
+        console.log("OK button correct4");
+        //彈出視窗CSS調整
+        jQuery('.modal-title').html('<h4>行事曆</h4>');
+        jQuery('.mod_styleSelect').hide();
+        jQuery('.mod_dynamicSelect').hide();
+        jQuery('.mod_FreeEditor').hide();
+        jQuery('.mod_Calendar').show();
+        jQuery('.mod_self_carousel').hide();
+
+        //特定載入功能
+        jQuery('.modal-footer').html('<button type="button" class="Calendar btn btn-primary float-right" id="Calendar">確定</button>');
+
+        //載入指定頁面- src/index/???.html
+        jQuery('.contact').load('src/dist/index.html');
+    });
+    //行事曆設定 - [結束]
+
+    //輪播設定 - [開始]
+    jQuery('[data-whatever="@self_carousel"]').click(function() {
+        console.log("OK button correct5");
+        //彈出視窗CSS調整
+        jQuery('.modal-title').html('<h4>輪播測試3</h4>');
+        jQuery('.mod_styleSelect').hide();
+        jQuery('.mod_dynamicSelect').hide();
+        jQuery('.mod_FreeEditor').hide();
+        jQuery('.mod_Calendar').hide();
+        jQuery('.mod_self_carousel').show();
+
+        //載入頁面-範例
+        //定義輪播工具
+        var carousel =
+            '<div id="carouselExampleControls" class="carousel slide" data-ride="carousel" data-interval="1000">\
+                <div class="carousel-inner" role="listbox" style=" height:480px; text-align:center; !important;">\
+                    <div class="carousel-item active">\
+                        <img id="img1" class="center-block w-auto h-480" style="height:480px" alt="First slide" src="http://ondinas.com.br/site/wp-content/themes/options/images/skins/headers/full_width/header-autumnBreeze.jpg">\
+                    </div>\
+                    <div class="carousel-item">\
+                        <img id="img2" class="center-block w-auto h-480" style="height:480px" alt="Second slide" src="http://www.boostnet.in/wp-content/uploads/2016/10/Header-1.png">\
+                    </div>\
+                    <div class="carousel-item">\
+                        <img id="img3" class="center-block w-auto h-480" style="height:480px" alt="Third slide" src="https://steklin.ru/wp-content/uploads/2019/12/Header-PNG-Pic.png">\
+                    </div>\
+                </div>\
+            </div>';
+
+        //上傳工具/功能
+        jQuery('.mod_self_carousel').html(
+            '<form runat="server" align="center">\
+                <img id="blah" src="#" />\
+                    <input type="file" id="imgInp" accept="image/gif, image/jpeg, image/png" />\
+            </form>');
+
+        $('.contact').html(carousel);
+
+        $(".carousel").carousel();
+        //特定載入功能
+        $('.modal-footer').html('<button type="button" class="imgupdate btn btn-primary float-right" id="imgupdate">確定嗎？</button>');
+        $("#imgInp").change(function() {
+            readURL(this);
+        });
+    });
+
+
+    //輪播設定 - [結束]
+});
